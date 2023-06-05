57dc2647701f5d43bb67adae2e311cd66ce4aeae
Nguyễn Phương Nam <npnam031101@gmail.com>
2023-06-05 17:33:55 +0700

    ok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // modules: [
-    //     'bootstrap-vue/nuxt'
-    // ]
-    css: [
-        "bootstrap/dist/css/bootstrap.css",
-        "bootstrap-vue-3/dist/bootstrap-vue-3.css",
-    ],
+
+
+
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,38 @@
 <template>
-  <h2>Home</h2>
+  <swiper :modules="[Virtual]" :slides-per-view="3" :space-between="50" virtual>
+    <swiper-slide
+      v-for="(slideContent, index) in slides"
+      :key="index"
+      :virtualIndex="index"
+      >{{ slideContent }}</swiper-slide
+    >
+  </swiper>
 </template>
-
 <script>
-export default {};
+import { Virtual } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+
+  setup() {
+    const slides = Array.from({ length: 1000 }).map(
+      (el, index) => `Slide ${index + 1}`
+    );
+    return {
+      slides,
+      Virtual,
+    };
+  },
+};
 </script>
 
-<style>
+<style >
+.swiper-slide {
+  background: blue;
+  border: 1px solid;
+}
 </style>
